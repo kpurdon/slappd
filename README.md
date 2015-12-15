f074a9f3dce21f0897f6b13fce8a62185e07b36f
kpurdon <kylepurdon@gmail.com>
2015-12-14 20:15:09 -0700

    Restructure for heroku deployment

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A [Slack](https://slack.com/) [slash command server](https://api.slack.com/slash
 ## Basic Usage
 
 1. Register for [Untappd API Credentials](https://untappd.com/api/register?register=new)
-2. Get the server running somewhere on a public host (TODO)
+2. Get the server running somewhere on a public host
 3. Configure a Slack Slash Command. [See Slack Docs](https://api.slack.com/slash-commands)
 
 The following environment variables must be set:
@@ -19,6 +19,7 @@ The following environment variables must be set:
 The server expects a POST request from Slack with the following form values:
 
 * token
+* user_name
 * text
 
 These values should be present in the default slack POST in addition to other non-used values.
@@ -27,9 +28,26 @@ These values should be present in the default slack POST in addition to other no
 
 The server will do a lookup on the Untappd API for the given `text` in the Slack POST. If no result is found an `ephemeral` (only displayed to requesting user) message will be returned stating that no results were found. If results are found an `in_channel` (displayed to anyone in the channel) message will be returned.
 
-# TODO ITEMS
+### Example
 
-* [] Complete Response Formatting
-* [] Testing
-* [] ?Support Other Untappd API Searches?
-* [] Documentation (Screenshots)
+The following request:
+
+```
+http://localhost:3000/?token=123Token456&user_name=kpurdon&text=Boulevard+Wheat
+```
+
+will generate the following JSON response:
+
+```
+{
+  response_type: "in_channel",
+  text: "Your Untappd Response",
+  attachments: [
+    {
+      title: "Unfiltered Wheat Beer",
+      text: "Boulevard Unfiltered Wheat Beer is a lively, refreshing ale with a     natural citrusy flavor and distinctive cloudy appearance. This easy drinking American-style wheat beer has become our most popular offering, and the best-selling craft beer in the Midwest.",
+      image_url: "https://untappd.akamaized.net/site/beer_logos/beer-_10501_sm_223bb99cc9949e694ada7d88309095.jpeg"
+    }
+  ]
+}
+```
---
+++ cmd/untappd/main.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+type SlackAttachment struct {
+	Title    string `json:"title,omitempty"`
+	Text     string `json:"text,omitempty"`
+	ImageURL string `json:"image_url,omitempty"`
+}
+
+type SlackResponse struct {
+	ResponseType string             `json:"response_type,omitempty"`
+	Text         string             `json:"text"`
+	Attachments  *[]SlackAttachment `json:"attachments,omitempty"`
+}
+
+type UntappdBeer struct {
+	Name        string  `json:"beer_name"`
+	Label       string  `json:"beer_label"`
+	Ibu         float64 `json:"beer_ibu"`
+	Abv         float64 `json:"beer_abv"`
+	Description string  `json:"beer_description"`
+}
+
+type UntappdBeerResponse struct {
+	Beers struct {
+		Items []struct {
+			Beer *UntappdBeer `json:"beer"`
+		}
+	}
+}
+
+type UntappdResponse struct {
+	Meta struct {
+		StatusCode int `json:"code"`
+	}
+	Beer *UntappdBeerResponse `json:"response"`
+}
+
+func untappdRequest(searchString string) (untappdData *UntappdResponse, err error) {
+
+	untappdResponse := &UntappdResponse{}
+
+	untappdClientID := os.Getenv("UNTAPPD_CLIENT_ID")
+	if untappdClientID == "" {
+		return untappdResponse, errors.New("unable to read environment variable UNTAPPD_CLIENT_ID")
+	}
+	untappdClientSecret := os.Getenv("UNTAPPD_CLIENT_SECRET")
+	if untappdClientSecret == "" {
+		return untappdResponse, errors.New("unable to read environment variable UNTAPPD_CLIENT_SECRET")
+	}
+
+	requestURL := url.URL{
+		Scheme: "https",
+		Host:   "api.untappd.com",
+		Path:   "/v4/search/beer",
+	}
+	q := requestURL.Query()
+	q.Set("client_id", untappdClientID)
+	q.Set("client_secret", untappdClientSecret)
+	q.Set("q", searchString)
+	requestURL.RawQuery = q.Encode()
+
+	res, err := http.Get(requestURL.String())
+	if err != nil {
+		return untappdResponse, err
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return untappdResponse, err
+	}
+	if err := json.Unmarshal(body, &untappdResponse); err != nil {
+		return untappdResponse, err
+	}
+
+	return untappdResponse, nil
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	slackToken := os.Getenv("SLACK_TOKEN")
+	if slackToken == "" {
+		log.Println("unable to read environment variable SLACK_TOKEN")
+		http.Error(w, "UNABLE_TO_READ_SLACK_TOKEN", http.StatusInternalServerError)
+		return
+	}
+
+	reqSlackToken := r.FormValue("token")
+	if reqSlackToken == "" {
+		http.Error(w, "MISSING_ARG_TOKEN", http.StatusBadRequest)
+		return
+	}
+
+	if slackToken != r.FormValue("token") {
+		http.Error(w, "INVALID_TOKEN", http.StatusUnauthorized)
+		return
+	}
+
+	reqUserName := r.FormValue("user_name")
+	if reqUserName == "" {
+		http.Error(w, "MISSING_ARG_USER_NAME", http.StatusBadRequest)
+		return
+	}
+	if reqUserName == "slackbot" {
+		return // this is a bot response that we simply want to ignore
+	}
+
+	searchString := r.FormValue("text")
+	if searchString == "" {
+		http.Error(w, "MISSING_ARG_TEXT", http.StatusBadRequest)
+		return
+	}
+
+	untappdData, err := untappdRequest(searchString)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	// set the default response data
+	response := &SlackResponse{
+		ResponseType: "in_channel",
+		Text:         "Your Untappd Response",
+	}
+
+	if len(untappdData.Beer.Beers.Items) == 0 {
+		response.ResponseType = "ephemeral"
+		response.Text = "No Results Found"
+	} else {
+		beer := untappdData.Beer.Beers.Items[0].Beer
+		newAttachment := SlackAttachment{
+			Title:    beer.Name,
+			Text:     beer.Description,
+			ImageURL: beer.Label,
+		}
+		attachments := append([]SlackAttachment{}, newAttachment)
+		response.Attachments = &attachments
+	}
+
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseJSON)
+}
+
+func main() {
+	var port string
+	port = os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+port, nil)
+}
